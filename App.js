92fd4ef51cd2919316a056a1ec71f9daa0c4eca1
DenilsonCunha <denilsonluciano6@gmail.com>
2021-06-22 21:54:49 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,3 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import {FontAwesome5} from '@expo/vector-icons';
+
+export default function App() {
+    return (
+      <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+
+   <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <View style={styles.container}>
+      <StatusBar backgroundColor='#FFF' translucent={false}/>
+
+      <Image
+       source={require('./src/assets/logo.png')}
+       style={styles.logo}
+     />
+
+<TextInput
+     placeholder="Celular, username ou email"
+     style={styles.input}
+     />
+     <TextInput
+     placeholder="Sua senha"
+     style={styles.input}
+     />
