6aa5db96ec8db3691e697352e20510a558269b8d
DenilsonCunha <denilsonluciano6@gmail.com>
2021-06-22 21:48:59 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import {FontAwesome5} from '@expo/vector-icons';
